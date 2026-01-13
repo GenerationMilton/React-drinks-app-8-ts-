e99ec04a83833417afb1cabf86d944b6b1c70695
Milton Munoz <livemilton@gmail.com>
2026-01-12 21:00:17 -0500

    Drinks searcher :sparkles: Placing the Recipe and its Content in the State

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,13 +1,14 @@
 import type { StateCreator } from "zustand";
 import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService";
-import type { Categories, Drink, Drinks, SearchFilter } from "../types";
+import type { Categories, Drink, Drinks, Recipe, SearchFilter } from "../types";
 
 export type RecipeSliceType = {
     categories: Categories;
-    drinks: Drinks
+    drinks: Drinks;
+    selectedRecipe: Recipe;
     fetchCategories: () => Promise<void>;
     searchRecipes: (searchFilter: SearchFilter) => Promise<void>;
-    selectRecipe:(id: Drink['idDrink']) => Promise<void>
+    selectRecipe:(id: Drink['idDrink']) => Promise<void>;
 }
 
 export const createRecipesSlice: StateCreator<RecipeSliceType> = (set) => ({
@@ -17,6 +18,7 @@ export const createRecipesSlice: StateCreator<RecipeSliceType> = (set) => ({
     drinks:{
         drinks:[]
     },
+    selectedRecipe: {} as Recipe,
     fetchCategories: async () => {
         const categories= await getCategories()
         set({
@@ -31,7 +33,9 @@ export const createRecipesSlice: StateCreator<RecipeSliceType> = (set) => ({
     },
     selectRecipe: async(id) => {
        const selectedRecipe = await getRecipeById(id)
-       console.log(selectedRecipe)
+       set({
+        selectedRecipe
+       })
     }
 
 })
