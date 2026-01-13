991d1c632619440ec234bf303c8a6a17ee016bc6
Milton Munoz <livemilton@gmail.com>
2026-01-12 20:47:03 -0500

    Drinks searcher :sparkles: Querying API to obtain the recipe details by Id

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,5 @@
 import type { StateCreator } from "zustand";
-import { getCategories, getRecipes } from "../services/RecipeService";
+import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService";
 import type { Categories, Drink, Drinks, SearchFilter } from "../types";
 
 export type RecipeSliceType = {
@@ -30,7 +30,8 @@ export const createRecipesSlice: StateCreator<RecipeSliceType> = (set) => ({
        })
     },
     selectRecipe: async(id) => {
-        console.log(id)
+       const selectedRecipe = await getRecipeById(id)
+       console.log(selectedRecipe)
     }
 
 })
