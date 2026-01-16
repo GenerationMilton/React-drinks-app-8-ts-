bb54367090f40cab9b5d4bc0bde0125e951f601b
Milton Munoz <livemilton@gmail.com>
2026-01-15 21:44:29 -0500

    Drinks searcher :sparkles: Add to favorites

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -1,10 +1,23 @@
-import { StateCreator} from 'zustand'
+import type { StateCreator} from 'zustand'
 import type { Recipe } from '../types'
 
 export type FavoriteSliceType ={
     favorites: Recipe[]
+    handleClickFavorite: (recipe: Recipe) => void
 }
 
-export const createFavoritesSlice : StateCreator<FavoriteSliceType> = () => ({
-    favorites:[]
+export const createFavoritesSlice : StateCreator<FavoriteSliceType> = (set, get) => ({
+    favorites:[],
+    handleClickFavorite: (recipe) =>{
+        if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)){
+            set((state)=>({
+                favorites: state.favorites.filter( favorite => favorite.idDrink !== recipe.idDrink)
+            }))
+        } else {
+        
+            set((state) => ({
+                favorites: [ ...state.favorites, recipe]
+            }))
+        }
+    }
 })
