ce647fa52bb6116355f2e1372c48cd4af15bb2ba
Milton Munoz <livemilton@gmail.com>
2026-01-15 22:53:27 -0500

    Drinks searcher :sparkles: Creating a slice for notifications

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,10 +3,12 @@ import { createRecipesSlice, type RecipeSliceType } from './recipeSlice';
 import { devtools } from 'zustand/middleware';
 import { createFavoritesSlice } from './favoritesSlice';
 import type {FavoriteSliceType} from './favoritesSlice';
+import { createNotificationSlice } from './notificationSlice';
+import type {NotificationSliceType} from './notificationSlice';
 
-
-export const useAppStore = create<RecipeSliceType & FavoriteSliceType>()(devtools( (...a)=> ({
+export const useAppStore = create<RecipeSliceType & FavoriteSliceType & NotificationSliceType>()(devtools( (...a)=> ({
     ...createRecipesSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a),
     
 })))
