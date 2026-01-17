c3ca30a67d89f4bad7b97e99c7e732d4d0d9636a
Milton Munoz <livemilton@gmail.com>
2026-01-16 20:58:29 -0500

    Drinks searcher :sparkles: Calling the Notification Slice to display it

--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -1,4 +1,6 @@
 import type { StateCreator} from 'zustand'
+import type { FavoriteSliceType } from './favoritesSlice'
+import type { RecipeSliceType } from './recipeSlice'
 
 type Notification = {
     text: string
@@ -8,13 +10,23 @@ type Notification = {
 
 export type NotificationSliceType = {
     notification: Notification
+    showNotification: (payload: Pick<Notification, 'text' | 'error'>) => void
 }
 
 
-export const createNotificationSlice : StateCreator<NotificationSliceType> = (set, get) => ({
+export const createNotificationSlice : StateCreator<NotificationSliceType & FavoriteSliceType,[],[],NotificationSliceType> = (set) => ({
     notification: {
         text: '',
         error: false,
         show: false,
+    },
+    showNotification: (payload) => {
+        set({
+            notification: {
+                text: payload.text,
+                error: payload.error,
+                show: true
+            }
+        })
     }
 })
