5e616545f6226a2db3b8179437d5cddaa9be9560
Milton Munoz <livemilton@gmail.com>
2026-01-16 21:55:18 -0500

    Drinks searcher :sparkles: Creating the AI Slice

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -5,10 +5,12 @@ import { createFavoritesSlice } from './favoritesSlice';
 import type {FavoriteSliceType} from './favoritesSlice';
 import { createNotificationSlice } from './notificationSlice';
 import type {NotificationSliceType} from './notificationSlice';
+import { createAISlice, type AISlice } from './aiSlice';
 
-export const useAppStore = create<RecipeSliceType & FavoriteSliceType & NotificationSliceType>()(devtools( (...a)=> ({
+export const useAppStore = create<RecipeSliceType & FavoriteSliceType & NotificationSliceType & AISlice>()(devtools( (...a)=> ({
     ...createRecipesSlice(...a),
     ...createFavoritesSlice(...a),
     ...createNotificationSlice(...a),
+    ...createAISlice(...a)
     
 })))
