78df81e9ecd86187fb172d2fb6547ba919bd302e
Milton Munoz <livemilton@gmail.com>
2026-01-16 21:35:15 -0500

    Drinks searcher :sparkles: performance in your apps with React Router DOM and Build

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import FavoritePage from "./views/FavoritePage"
-import IndexPage from "./views/IndexPage"
 import Layout from "./layouts/Layout"
+import { lazy, Suspense } from "react"
+
+const IndexPage = lazy(()=> import('./views/IndexPage'))
+const FavoritesPage = lazy(()=> import( './views/FavoritePage'))
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
-            <Route path='/' element={<IndexPage />} index />
-            <Route path='/favoritos' element={<FavoritePage />} />
+            <Route path='/' element={
+              <Suspense fallback="Cargando...">
+                <IndexPage />
+              </Suspense>
+            } index />
+            <Route path='/favoritos' element={
+              <Suspense fallback="Cargando...">
+                <FavoritesPage />
+              </Suspense>
+            } />
           </Route>
         </Routes>
     </BrowserRouter>
