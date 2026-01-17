8baf2db97464698e8e4f4a0489ea19934ed98c02
Milton Munoz <livemilton@gmail.com>
2026-01-16 21:05:23 -0500

    Drinks searcher :sparkles: Closing notification

--- src/stores/notificationSlice.ts
+++ src/stores/notificationSlice.ts
@@ -11,10 +11,11 @@ type Notification = {
 export type NotificationSliceType = {
     notification: Notification
     showNotification: (payload: Pick<Notification, 'text' | 'error'>) => void
+    hideNotification: () => void
 }
 
 
-export const createNotificationSlice : StateCreator<NotificationSliceType & FavoriteSliceType,[],[],NotificationSliceType> = (set) => ({
+export const createNotificationSlice : StateCreator<NotificationSliceType & FavoriteSliceType,[],[],NotificationSliceType> = (set, get) => ({
     notification: {
         text: '',
         error: false,
@@ -28,5 +29,17 @@ export const createNotificationSlice : StateCreator<NotificationSliceType & Favo
                 show: true
             }
         })
+        setTimeout(()=>{
+            get().hideNotification()
+        }, 5000);
+    },
+    hideNotification: () =>{
+         set({
+            notification: {
+                text: '',
+                error: false,
+                show: false,
+            },
+         })
     }
 })
