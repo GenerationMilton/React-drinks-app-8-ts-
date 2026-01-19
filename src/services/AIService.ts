e0e3ef75d5d665f1861b4098912c7a5061ecb087
Milton Munoz <livemilton@gmail.com>
2026-01-18 20:12:53 -0500

    Drinks searcher :sparkles: Adding behavior to AI

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -5,15 +5,16 @@ export default {
     
     async generateRecipe(promt: string) {
         console.log(promt);
-        //console.log("👉 KEY:", import.meta.env.VITE_OPENROUTER_KEY);
-    
+        
         const openrouter = createOpenRouter({
             apiKey: import.meta.env.VITE_OPENROUTER_KEY,
         });
         
         const result = streamText({
             model: openrouter.chat('meta-llama/llama-3.2-3b-instruct:free'),
-            prompt: promt
+            prompt: promt,
+            system: 'Eres un bartender que tiene 50 años de experiencia y le sirvio una bebida a James Bond',
+            temperature: 0.2
         });
     
         console.log(result.textStream);
