361c90791ccc813a6ec4901bc3cd90f70e1fbe37
Milton Munoz <livemilton@gmail.com>
2026-01-18 19:11:25 -0500

    Drinks searcher :sparkles: Sending requests to the AI and reading the responses

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -4,7 +4,11 @@ import { openrouter } from '../lib/ai'
 export default {
     async generateRecipe(prompt: string){
         const result = streamText({
-            model: openrouter('')
+            model: openrouter('meta-llama/llama-3.3-70b-instruct:free'),
+            prompt: prompt
         })
+
+        console.log(result)
+        return result.textStream
     }
 }
